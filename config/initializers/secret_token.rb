cd0d936393497212d90bb610210d2eeaae0517bb
jatin-baweja <jatin.baweja@vinsol.com>
2014-06-26 15:07:27 +0530

    Initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store210::Application.config.secret_key_base = 'c65b4150deee8c56a01d420bf2d3f60158b2559efe3b1a6bb801173daa99fb6bf54311389fc3acd2b0199476dff68d968519cbac5411ca5e6642fa965c6fcd1b'
